e155a19df086600957f4830a07a6c9bd83e06f52
Ravi Theja <ravi03071991@gmail.com>
2023-02-09 11:48:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,52 @@
 import streamlit as st
-import openai
+import pinecone
+import utils
 
-st.set_page_config(page_title="Headline Generation",
+pinecone.init(api_key=utils.pinecone_api_key,
+              environment=utils.pinecone_environment)
+
+st.set_page_config(page_title="GodsAPP",
                    page_icon=":guardsman:", layout="wide")
-st.header("Headline Generation")
-st.write("This app will generate headlines based on text with limits on number of characters and words you need in the headline.")
+st.header("GodsAPP")
+st.write("GodsAPP will give answers based on Bhagwad Gita, Quran and Bible for your situation")
 
+tab1, tab2, tab3 = st.tabs(["Bhagwad Gita", "Quran", "Bible"])
 
-def generate(text, num_chars, num_words):
+with tab1:
+    st.header("Bhagwad Gita")
+    title = st.text_input(
+        'Enter Your Situation for Bhagwad Gita to suggest a solution')
+    if st.button('Ask Gita'):
+        st.write("Bhagwad Gita is thinking. Please wait.")
+        answer = utils.qa("gita", title)
+        answer, relevantverses = answer.split("----------------")
+        st.subheader('Answer')
+        st.write(answer)
+        st.subheader('Relevant Verses')
+        st.write(relevantverses)
 
-    openai.api_key = "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"
-    query = f"Generate a news headline that will attract users attention strict to maximum of {num_chars} characters and {num_words} words without : based on the context of the following text. Don't hallicunate."
+with tab2:
+    st.header("Quran")
+    title = st.text_input(
+        'Enter Your Situation for Quran to suggest a solution')
+    if st.button('Ask Quran'):
+        st.write("Quran is thinking. Please wait.")
+        answer = utils.qa("quran", title)
+        answer, relevantverses = answer.split("----------------")
+        st.subheader('Answer')
+        st.write(answer)
+        st.subheader('Relevant Verses')
+        st.write(relevantverses)
 
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=query + "\n\n" + text + "\n\n",
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-
-    if response['choices'][0]["text"] != '':
-        return response['choices'][0]["text"]
-
-    return text
-
-
-text = st.text_input(
-    'Enter Your Text', max_chars=1000)
-charlimit = st.text_input(
-    'Enter Number of Characters')
-wordlimit = st.text_input(
-    'Enter Number of Words Text')
-if st.button('Submit'):
-    st.write("Processing. Please wait.")
-    answer = generate(text, charlimit, wordlimit)
-    st.subheader('Generated Headline')
-    st.write(answer)
+with tab3:
+    st.header("Bible")
+    title = st.text_input(
+        'Enter Your Situation for Bible to suggest a solution')
+    if st.button('Ask Bible'):
+        st.write("Bible is thinking. Please wait.")
+        answer = utils.qa("bible", title)
+        answer, relevantverses = answer.split("----------------")
+        st.subheader('Answer')
+        st.write(answer)
+        st.subheader('Relevant Verses')
+        st.write(relevantverses)
